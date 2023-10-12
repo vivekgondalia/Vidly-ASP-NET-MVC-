bf1a39548e85ec68f9395f886906ff3ed806126f
Vivek Gondalia <vgondalia@drintl.com>
2023-10-12 19:02:47 -0400

    Movie Web API - Build Basic Skeleton

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private MyDBContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDBContext();
+        }
+
+        //GET /api/movies
+        public IHttpActionResult GetMovies()
+        {
+            return Ok();
+        }
+
+        //GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            return Ok();
+        }
+
+        //POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(Movie movie)
+        {
+            return Ok();
+        }
+
+        //PUT /api/movies/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, Movie movie)
+        {
+            return Ok();
+        }
+
+        //DELETE /api/movies/1
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            return Ok();
+        }
+    }
+}
